218a4e5264f04d73a6a2bbea15b08c61c9486822
y-song <y.song@berkeley.edu>
2026-09-23 22:49:14 +0000

    For comparing combined jet pT before and after matching to pp

--- alian/plot/plot_data_pt.C
+++ alian/plot/plot_data_pt.C
@@ -84,27 +84,28 @@ void plot_data_pt(std::string file_name)
 {
     SetStyle();
     
-    std::string infile  = "/rstorage/youqi/" + file_name + "/AnalysisResultsFinal.root";
+    std::string infile  = "/home/youqi/alian/alian/output/embed.root";//"/rstorage/youqi/" + file_name + "/AnalysisResultsFinal.root";
     TFile *f = new TFile(infile.c_str(), "READ");
 
-    TH1D *h_JetPt_pre_select = (TH1D *)f->Get("jet_pT");
+    TH1D *h_sub_pT_all = (TH1D *)f->Get("sub_pT");
+    // TH1D *h_sub_pT_all = (TH1D *)f->Get("pp_pT"); // for pp
 
-    TH2 *h_JetPt_SubPt = (TH2 *)f->Get("jet_pT_sub_pT_post_selection");
-    TH2 *h_JetPt_SubPt_cut = (TH2 *)h_JetPt_SubPt->Clone(Form("%s_cut", h_JetPt_SubPt->GetName()));
-    // TH1D *h_JetPt = h_JetPt_SubPt_cut->ProjectionX(Form("%s_projx", h_JetPt_SubPt_cut->GetName()));
-    TH1D *h_SubPt = h_JetPt_SubPt_cut->ProjectionY(Form("%s_projy", h_JetPt_SubPt_cut->GetName()));
+    TH2 *h_combined_pT_sub_pT_matched = (TH2 *)f->Get("combined_pT_sub_pT_matched");
+    TH1D *h_sub_pT_matched = h_combined_pT_sub_pT_matched->ProjectionX(Form("%s_projx", h_combined_pT_sub_pT_matched->GetName()));
+    // TH2 *h_combined_pT_sub_pT_matched = (TH2 *)f->Get("sub_pT_pp_pT_matched"); // for pp
+    // TH1D *h_sub_pT_matched = h_combined_pT_sub_pT_matched->ProjectionX(Form("%s_projx", h_combined_pT_sub_pT_matched->GetName())); // for pp
 
     TCanvas *c = new TCanvas("c", "c", 700, 500);
     ProcessCanvas(c);
     c->cd();
     gPad->SetLogy();
     
-    FormatHist(h_JetPt_pre_select, kBlue, kFullSquare);
+    FormatHist(h_sub_pT_all, kBlue, kFullSquare);
     // FormatHist(h_JetPt, kBlue, kFullTriangleUp);
-    FormatHist(h_SubPt, kRed, kFullCircle);
-    h_JetPt_pre_select->Draw();
+    FormatHist(h_sub_pT_matched, kRed, kFullCircle);
+    h_sub_pT_all->Draw();
     // h_JetPt->Draw("same");
-    h_SubPt->Draw("same");
+    h_sub_pT_matched->Draw("same");
 
-    c->SaveAs("output/data_pt.png");
+    c->SaveAs("output/embed_pt.png");
 }
